579bdce8fbd97fd605ad0b3bf61dc0cbbb2d8bac
kingnambi <raphael.nambiar@gmail.com>
2016-05-17 22:51:06 +0200

    little movement adjustments; grid is ok now.
    changed GetKey into GetKeyDown. the platform moves now just 1f at a
    klick on the button.

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 
-    private float movingDistance = 0.2f;
+    private float movingDistance = 1f;
 
     //Maximale Auslenkung beim Bewegen in die vier Richtungen.
     private float maxUpDistance = 1.1f;
@@ -23,7 +23,7 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             if (maxUpDistance - movingDistance >= transform.position.z)
             {
@@ -33,21 +33,21 @@ public class PlayerController : MonoBehaviour {
             }
         }
 
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             if (maxDownDistance + movingDistance <= transform.position.z)
             {
                 transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - movingDistance);
             }
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             if (maxLeftDistance + movingDistance <= transform.position.x)
             {
                 transform.position = new Vector3(transform.position.x - movingDistance, transform.position.y, transform.position.z);
             }
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         { 
             if (maxRightDistance - movingDistance >= transform.position.x) 
                 {
