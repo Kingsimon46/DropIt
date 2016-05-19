59f651e0bb2aa5d4822489a1c9caf40b3dde3395
kingnambi <raphael.nambiar@gmail.com>
2016-05-19 20:43:37 +0200

    ObjectPooler added
    added the Objecpooler.

--- Assets/scripts/ObjectSpawner.cs
+++ Assets/scripts/ObjectSpawner.cs
@@ -1,22 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ObjectSpawner : MonoBehaviour {
 
-    public GameObject spawnPoint;
+    public GameObject ObjectToSpawn;
+    public Transform generationPoint;
+    public ObjectPooler theObjectPool;
+    
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+       
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
         
-	}
+
+
+    }
 
     void spawnNewDrop()
     {
-        
+        transform.position = new Vector3(generationPoint.transform.position.x, generationPoint.transform.position.y, generationPoint.transform.position.z);
+        //without Objectpooler//
+        //Instantiate (ObjectToSpawn, generationPoint.transform.position,transform.rotation);
+
+        GameObject newObject = theObjectPool.GetPooledObject();
+        newObject.transform.position = generationPoint.transform.position;
+        newObject.transform.rotation = generationPoint.transform.rotation;
+        newObject.SetActive(true);
     }
+       
+
 }
