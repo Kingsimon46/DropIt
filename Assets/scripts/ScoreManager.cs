052f226435b9dcb8ced90a642270ac7f77bcdb52
kingnambi <raphael.nambiar@gmail.com>
2016-05-26 20:09:05 +0200

    ScoreManager started.
    ScoreManager started.
    had to commit cauz of simi.

---
+++ Assets/scripts/ScoreManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public Text scoreText;
+    public Text highScoreText;
+
+    private float scoreCount;
+    public float highScoreCount;
+
+    public float pointsPerDrop;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        scoreText.text = "Score: " + scoreCount;
+
+        if(scoreCount > highScoreCount)
+        {
+            highScoreCount = scoreCount;
+
+        }
+
+
+        highScoreText.text = "High Score: " + highScoreCount;
+
+	}
+    
+    public void AddScore()
+    {
+        scoreCount =+ pointsPerDrop;
+
+    }
+}
